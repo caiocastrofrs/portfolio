c3b03a4e0830684b82a9483d6b5746f16a632939
Caio Castro <caiocastrofrs@gmail.com>
2026-06-09 18:03:43 -0300

    add other html files into building

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        projects: resolve(__dirname, "projects.html"),
+        blog: resolve(__dirname, "blog.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
+  },
 });
