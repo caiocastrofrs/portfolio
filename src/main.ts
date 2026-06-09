1624b1b705007ce9fa83eaeb6a1c2d4eb33da64f
Caio Castro <caiocastrofrs@gmail.com>
2026-06-09 18:12:59 -0300

    change title and add y on age

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ const age = Math.floor(
   Math.abs(Number(new Date()) - Number(new Date(1999, 2, 12))) / (1000 * 60 * 60 * 24) / 365,
 );
 
-document.querySelector<HTMLDivElement>("#age")!.innerHTML = String(age);
+document.querySelector<HTMLDivElement>("#age")!.innerHTML = String(age) + "y";
